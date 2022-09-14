0688530f44bbc2bd0eb6350c98730c88ba42eebb
diyameeniga <diyariti1@gmail.com>
2022-09-14 09:57:14 -0500

    9/14 edits

--- MyFirstProject/Models/Titles.cs
+++ MyFirstProject/Models/Titles.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace MyFirstProject.Models
-{
+{ 
     public static class Titles
     {
+       //MAIN
+        public static string MainPageTitle { get; } = "Main Page";
+
+        public static string MainPageSubtitle { get; } = "My Mobile App";
+
+        //   public static string DataBindingTitle { get; internal set; }
+
+
     }
 }
---
+++ MyFirstProject/ViewViewModels/Main/MainPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MyFirstProject.Models;
+using MyFirstProject.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstProject.ViewViewModels.Main
+{
+    public class MainPageViewModel: BaseViewModel
+    {
+        string subtitle = string.Empty;
+        public string Subtitle
+        {
+            get { return subtitle; }
+            set { SetProperty(ref subtitle, value); }
+        }
+
+        public MainPageViewModel()
+        {
+            Title = Titles.MainPageTitle;
+            Subtitle = Titles.MainPageSubtitle;
+        }
+    }
+}
