26e8c9733851c60d6717534a1fb5ef6140a14a6b
diyameeniga <diyariti1@gmail.com>
2023-01-20 11:12:22 -0600

    1/20 changes

--- MyFirstProject/ViewViewModels/Controls/Picker/PickerViewModel.cs
+++ MyFirstProject/ViewViewModels/Controls/Picker/PickerViewModel.cs
@@ -10,6 +10,7 @@ namespace MyFirstProject.ViewViewModels.Controls.Picker
     class PickerViewModel: BaseViewModel
     {
         public ImageSource SubmitButton { get; set; }
+
         public PickerViewModel()
         {
             Title = Titles.PickerViewTitle;
